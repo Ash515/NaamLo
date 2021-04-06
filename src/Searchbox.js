d171f6d542da9866c927da36b1ea5b5d06029651
Ashwin Kumar Ramaswamy <aaashwin515@gmail.com>
2021-04-06 23:13:54 +0530

    Add files via upload

---
+++ src/Searchbox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Searchbox.css';
+const Searchbox=()=>{
+    return(
+    <div className="search-container">
+      <input onChange={(event)=>{
+          
+      }} placeholder="Type keywords" className="search-input"/>
+
+    </div>
+    );
+};
+export default Searchbox;
